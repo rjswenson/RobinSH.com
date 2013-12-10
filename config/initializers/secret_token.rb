7c2dd5e482b1a02fd3447461fc8db89f8476e01c
Robin Swenson-Healey <rjswenson@gmail.com>
2013-12-10 11:26:23 -0800

    figaro obfuscation

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PersonalSite::Application.config.secret_key_base = 'e6339f75997e85593345b7026c23572b850efcb7b4c5e82fea7bd4c08fdec611085ca3ffd8647e4d6012b19626c82d63e769fabd037db6b5375cc217c0ea6f71'
+PersonalSite::Application.config.secret_key_base = ENV['SECRET_TOKEN']
