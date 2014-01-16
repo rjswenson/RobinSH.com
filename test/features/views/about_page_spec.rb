afef6cbcb52b28aa6fd974877bb31b2d168eb76b
Robin Swenson-Healey <rjswenson@gmail.com>
2014-01-16 10:34:31 -0800

    contact page tests stubbed out

---
+++ test/features/views/about_page_spec.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+feature "The about page" do
+  scenario "Has a title" do
+    pending
+  end
+
+  scenario "Has info about me" do
+    pending
+  end
+
+  scenario "Has my contact information" do
+    pending
+  end
+
+  scenario "Has my social links" do
+    pending
+  end
+end
