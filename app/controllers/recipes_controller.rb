fa0eb30dfc0fae3756449122594c48978d07fbfb
Robin Swenson-Healey <rjswenson@gmail.com>
2014-01-23 14:40:19 -0800

    basic CRUD for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,39 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:index, :show]
+
+  def show
+  end
+
+  def index
+    @recipes = Recipe.order("created_at DESC").paginate(page: params[:page], per_page: 1)
+    @all_recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:dish, :html)
+  end
 end
